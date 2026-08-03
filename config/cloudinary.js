1f908b24cbf67bbdda954f3b7d320fed507b2711
Yahya Hussain <myahyabd20@gmeil.com>
2026-08-03 08:55:57 +0600

    Configure fallback Cloudinary credentials in case env vars are missing on Vercel, and clean up test-config endpoint

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 
-console.log('Cloudinary Loaded:', process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME);
+console.log('Cloudinary Loaded:', process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME || 'daf21yu47');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY || process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET || process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || process.env.CLOUD_NAME || 'daf21yu47',
+  api_key: process.env.CLOUDINARY_API_KEY || process.env.CLOUD_API_KEY || '432154868979178',
+  api_secret: process.env.CLOUDINARY_API_SECRET || process.env.CLOUD_API_SECRET || 'B1ihtDLGi866VkSzJHdp10mzS60',
 });
 
 module.exports = cloudinary;
